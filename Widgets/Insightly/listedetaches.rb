b9788ccbb56cb2cf94abe4e3b085fbecb462cb8d
belcaid <belcaid.mehdi@gmail.com>
2014-11-19 07:55:21 -0500

    Update listedetaches.rb

--- Widgets/Insightly/listedetaches.rb
+++ Widgets/Insightly/listedetaches.rb
@@ -1,24 +1,11 @@
 require 'insightly'
 require 'json'
-
-module Insightly
-  autoload :Client, 'insightly/client'
-  autoload :DSL, 'insightly/dsl'
-  autoload :Resources, 'insightly/resources'
-  autoload :Errors, 'insightly/errors'
-  autoload :Utils, 'insightly/utils'
-
-  class << self
-    # @return [String]
-    attr_accessor :api_key
-    attr_accessor :logger
-  end
-
-  module_function
-end
+require "net/http"
 
 # Api Insightly se trouvant dans le profil de l'utilisateur
 Insightly.api_key = '<your API key>'
+
+
 
 
 
