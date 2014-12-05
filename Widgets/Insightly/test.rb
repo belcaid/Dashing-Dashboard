25a0fe1304674e88a2271c66f983d1ae560fad88
belcaid <belcaid.mehdi@gmail.com>
2014-12-04 21:04:50 -0500

    Update test.rb

--- Widgets/Insightly/test.rb
+++ Widgets/Insightly/test.rb
@@ -1,16 +1,15 @@
 require 'insightly'
 
+module insightly
 # Api Insightly se trouvant dans le profil de l'utilisateur
 Insightly.api_key = '<your API key>'
-
+contact = Insightly.client.get_contact(id: 1)
+end
 
 
 # Lancement du rafraichissement
 SCHEDULER.every '1m', :first_in => 0 do
 
-contact = Insightly.client.get_contact(id: 1)
-  
- 
   # Update the dashboard
-  send_event('listedetaches',   { items: contact })
+  send_event('test',   { items: contact })
 end
